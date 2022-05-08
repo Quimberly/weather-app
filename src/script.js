b64d6eed947b15c0baaa759208ff6eaba17e2e14
Quimberly <96629097+Quimberly@users.noreply.github.com>
2022-05-08 23:15:15 +0200

    Update current Location

--- src/script.js
+++ src/script.js
@@ -20,6 +20,18 @@ function formatDate(timestamp) {
   let day = days[date.getDay()];
   return `${day} ${hours}:${minutes}`;
 }
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
+}
+
+function getForecast(coordinates) {
+  let apiKey = "6d14bf3fa5db85996e16c81f2734587e";
+  let apiurl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiurl).then(displayForecast);
+}
 
 function showWeather(response) {
   console.log(response.data);
@@ -41,6 +53,8 @@ function showWeather(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 
 function search(city) {
