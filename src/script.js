353989a22f1bc046ad4c8dc5e2eadbffbcd3b544
Quimberly <96629097+Quimberly@users.noreply.github.com>
2022-05-15 19:15:31 +0200

    Adding forecast js

--- src/script.js
+++ src/script.js
@@ -35,7 +35,32 @@ function getForecast(coordinates) {
 }
 
 function displayForecast(response) {
-  console.log(response.data);
+  console.log(response.data.daily);
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  let forcastHTML = `<div class="row">`;
+  days.forEach(function (day) {
+    forcastHTML =
+      forcastHTML +
+      ` <div class="col-2">
+        <div class="weather-forecast-date">${day}</div>
+        <img
+          src="http://openweathermap.org/img/wn/50d@2x.png"
+          alt=""
+          width="42"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> 18° </span>
+          <span class="weather-forecast-temperature-min"> 12° </span>
+        </div>
+      </div>
+    `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
 }
 
 function showWeather(response) {
